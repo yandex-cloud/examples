d445307f197d232009525984e8962a7ac5102765
deka <45845435+userdeka@users.noreply.github.com>
2019-09-03 14:00:26 +0300

    Create example for Alice

---
+++ serverless/functions/alice/nodejs/parrot/index.js
@@ -0,0 +1,27 @@
+/**
+ * Entry-point for serverless function.
+ * Since we export only handler itself use `index` as function entrypoint parameter.
+ *
+ * @param event {Object} request payload.
+ * @param context {Object} information about current execution context.
+ *
+ * @return {Promise<Object>} response to be serialized as JSON.
+ */
+module.exports.handler = async (event, context) => {
+    const {version, session, request} = event;
+
+    let text = 'Hello! I\'ll repeat anything you say to me.';
+    if (request['original_utterance'].length > 0)
+        text = request['original_utterance'];
+    return {
+        version,
+        session,
+        response: {
+            // Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
+            text: text,
+
+            // Don't finish the session after this response.
+            end_session: false,
+        },
+    };
+};
