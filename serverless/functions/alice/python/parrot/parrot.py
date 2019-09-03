8372030b03e6a09edc07db70d3a894dbcc7c1c3d
deka <45845435+userdeka@users.noreply.github.com>
2019-09-03 14:01:41 +0300

    Create examples for Alice

---
+++ serverless/functions/alice/python/parrot/parrot.py
@@ -0,0 +1,23 @@
+def handler(event, context):
+    """
+    Entry-point for serverless function.
+    Use 'parrot.handler' as function entrypoint parameter.
+    :param event: request payload.
+    :param context: information about current execution context.
+    :return: response to be serialized as JSON.
+    """
+    text = 'Hello! I\'ll repeat anything you say to me.'
+    if 'request' in event and \
+            'original_utterance' in event['request'] \
+            and len(event['request']['original_utterance']) > 0:
+        text = event['request']['original_utterance']
+    return {
+        'version': event['version'],
+        'session': event['session'],
+        'response': {
+            # Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
+            'text': text,
+            # Don't finish the session after this response.
+            'end_session': 'false'
+        },
+    }
