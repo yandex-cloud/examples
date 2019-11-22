b9698f950ef7dfcf45cd41d190a3090f2b86b668
Yuri Molodtsov <l2k1@yandex-team.ru>
2019-11-22 19:28:30 +0300

    Implemented code for auth with service account

--- kms/quickstart/kms-client.sh
+++ kms/quickstart/kms-client.sh
@@ -4,6 +4,16 @@ if [[ "$DEBUG" == 1 ]]; then
   set -x
 fi
 
+MEDATADA_ADDRESS=169.254.169.254
+GCE_SERVICE_ACCOUNT_PATH=computeMetadata/v1/instance/service-accounts/default/token
+SA_TOKEN_URL=$MEDATADA_ADDRESS/$GCE_SERVICE_ACCOUNT_PATH
+GCE_HEADERS='-H "Metadata-Flavor: Google"'
+SA_IAM_TOKEN=$(curl --connect-timeout 1 -s $GCE_HEADERS $SA_TOKEN_URL | jq ".access_token" -r)
+if [ -z "$SA_IAM_TOKEN" ]; then
+  IAM_TOKEN=${IAM_TOKEN:-$(yc iam create-token)}
+else
+  IAM_TOKEN=${IAM_TOKEN:-$SA_IAM_TOKEN}
+fi
 IAM_TOKEN=${IAM_TOKEN:-$(yc iam create-token)}
 KMS_HTTP_ENDPOINT=${KMS_HTTP_ENDPOINT:-"https://kms.yandex:443"}
 
