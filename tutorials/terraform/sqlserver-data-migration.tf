328bcb8fc4c8ef6a8c5d3ef2710565f619ecda83
maxdunaevsky <maxdunaevsky@yandex-team.ru>
2022-06-06 16:16:20 +0300

    Updated sqlserver-data-migration.tf

--- tutorials/terraform/sqlserver-data-migration.tf
+++ tutorials/terraform/sqlserver-data-migration.tf
@@ -1,15 +1,16 @@
-# Infrastructure for Yandex Cloud Managed Service for SQL Server
+# Infrastructure for Yandex Cloud Managed Service for SQL Server.
 #
 # RU: https://cloud.yandex.ru/docs/managed-sqlserver/tutorials/data-migration
 # EN: https://cloud.yandex.com/en/docs/managed-sqlserver/tutorials/data-migration
 #
 # Set the configuration of the Managed Service for SQL Server cluster:
 locals {
-  sql_server_version = "" # Set the SQL Server version. Мust be the same or higher than the version in the source cluster.
-  db_name            = "" # Set a database name
-  username           = "" # Set a user name
-  password           = "" # Set a user password
+  sql_server_version = "" # Set the SQL Server version. It must be the same or higher than the version in the source cluster.
+  db_name            = "" # Set a database name.
+  username           = "" # Set a user name.
+  password           = "" # Set a user password.
 }
+# For Migration using Logical import add users who are in the source database and use SQL Server Authentication, with the same names and passwords. Look line 74.
 
 resource "yandex_vpc_network" "network" {
   description = "Network for the Managed Service for SQL Server cluster"
@@ -17,7 +18,7 @@ resource "yandex_vpc_network" "network" {
 }
 
 resource "yandex_vpc_subnet" "subnet-a" {
-  description    = "Subnet in ru-central1-a availability zone"
+  description    = "Subnet in the ru-central1-a availability zone"
   name           = "subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
@@ -32,14 +33,6 @@ resource "yandex_vpc_security_group" "security-group" {
     description    = "Allow connections to SQL Server from the Internet"
     protocol       = "TCP"
     port           = 1433
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description    = "Allow outgoing connections to any required resource"
-    protocol       = "ANY"
-    from_port      = 0
-    to_port        = 65535
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -61,7 +54,7 @@ resource "yandex_mdb_sqlserver_cluster" "sqlserver-cluster" {
   host {
     zone             = "ru-central1-a"
     subnet_id        = yandex_vpc_subnet.subnet-a.id
-    assign_public_ip = true # Required for connection from the Internet
+    assign_public_ip = true # Required for connection from the Internet.
   }
 
   database {
@@ -77,4 +70,10 @@ resource "yandex_mdb_sqlserver_cluster" "sqlserver-cluster" {
       roles         = ["OWNER"]
     }
   }
+
+  # For Migration using Logical import uncomment, multiply this block and аdd users who are in the source database and use **SQL Server Authentication**, with the same names and passwords.
+  #  user {
+  #    name     =
+  #    password =
+  #  }
 }
