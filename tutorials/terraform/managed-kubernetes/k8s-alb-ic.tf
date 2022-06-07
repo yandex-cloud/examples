882b318fea23c6c43b04336e19a2bf2b810cb7db
vibelyakov <vibelyakov@yandex-team.ru>
2022-06-07 17:28:08 +0400

    YCDOCIO-1617: edit after review

--- tutorials/terraform/managed-kubernetes/k8s-alb-ic.tf
+++ tutorials/terraform/managed-kubernetes/k8s-alb-ic.tf
@@ -5,8 +5,7 @@
 locals {
   folder_id              = ""            # Set your cloud folder ID.
   k8s_node_group_version = "1.20"        # Set the version of Kubernetes for the node group.
-  k8s_cluster_version    = "1.21"        # Set the version of Kubernetes for the master host.
-  zone_a_v4_cidr_blocks  = "10.1.0.0/16" # Set the CIDR block for the Subnet in ru-central1-a availability zone.
+  k8s_cluster_version    = "1.20"        # Set the version of Kubernetes for the master host.
 }
 
 variable "zone_a_v4_cidr_blocks" {
@@ -15,12 +14,12 @@ variable "zone_a_v4_cidr_blocks" {
 }
 
 resource "yandex_vpc_network" "k8s-network" {
-  description = "Network for the Managed Service for Kubernetes cluster."
+  description = "Network for the Managed Service for the Managed Service for Kubernetes cluster"
   name        = "k8s-network"
 }
 
 resource "yandex_vpc_subnet" "subnet-a" {
-  description    = "Subnet in ru-central1-a availability zone."
+  description    = "Subnet in ru-central1-a availability zone"
   name           = "subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.k8s-network.id
@@ -28,12 +27,12 @@ resource "yandex_vpc_subnet" "subnet-a" {
 }
 
 resource "yandex_vpc_security_group" "k8s-main-sg" {
-  description = "Group rules ensure the basic performance of the cluster. Apply it to the cluster and node groups."
+  description = "Group rules ensure the basic performance of the cluster. Apply it to the cluster and node groups"
   name        = "k8s-main-sg"
   network_id  = yandex_vpc_network.k8s-network.id
 
   ingress {
-    description       = "The rule allows availability checks from the load balancer's range of addresses. It is required for the operation of a fault-tolerant cluster and load balancer services."
+    description       = "The rule allows availability checks from the load balancer's range of addresses. It is required for the operation of a fault-tolerant cluster and load balancer services"
     protocol          = "TCP"
     predefined_target = "loadbalancer_healthchecks"
     from_port         = 0
@@ -41,7 +40,7 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
   }
 
   ingress {
-    description       = "The rule allows incoming TCP-connections on ports 10501 and 10502 from the load balancer's range of addresses."
+    description       = "The rule allows incoming TCP-connections on ports 10501 and 10502 from the load balancer's range of addresses"
     protocol          = "TCP"
     predefined_target = "loadbalancer_healthchecks"
     from_port         = 10501
@@ -49,7 +48,7 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
   }
 
   ingress {
-    description       = "The rule allows the master-node and node-node interaction within the security group."
+    description       = "The rule allows the master-node and node-node interaction within the security group"
     protocol          = "ANY"
     predefined_target = "self_security_group"
     from_port         = 0
@@ -57,7 +56,7 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
   }
 
   ingress {
-    description    = "The rule allows the pod-pod and service-service interaction. Specify the subnets of your cluster and services."
+    description    = "The rule allows the pod-pod and service-service interaction. Specify the subnets of your cluster and services"
     protocol       = "ANY"
     v4_cidr_blocks = [var.zone_a_v4_cidr_blocks]
     from_port      = 0
@@ -65,27 +64,27 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
   }
 
   ingress {
-    description    = "The rule allows receipt of debugging ICMP packets from internal subnets."
+    description    = "The rule allows receipt of debugging ICMP packets from internal subnets"
     protocol       = "ICMP"
     v4_cidr_blocks = [var.zone_a_v4_cidr_blocks]
   }
 
   ingress {
-    description    = "The rule allows connection to Kubernetes API on 6443 port from the Internet."
+    description    = "The rule allows connection to Kubernetes API on 6443 port from the Internet"
     protocol       = "TCP"
     v4_cidr_blocks = ["0.0.0.0/0"]
     port           = 6443
   }
 
   ingress {
-    description    = "The rule allows connection to Kubernetes API on 443 port from the Internet."
+    description    = "The rule allows connection to Kubernetes API on 443 port from the Internet"
     protocol       = "TCP"
     v4_cidr_blocks = ["0.0.0.0/0"]
     port           = 443
   }
 
   egress {
-    description    = "The rule allows all outgoing traffic. Nodes can connect to Yandex Container Registry, Object Storage, Docker Hub, and more."
+    description    = "The rule allows all outgoing traffic. Nodes can connect to Yandex Container Registry, Object Storage, Docker Hub, and more"
     protocol       = "ANY"
     v4_cidr_blocks = ["0.0.0.0/0"]
     from_port      = 0
@@ -94,7 +93,7 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
 }
 
 resource "yandex_iam_service_account" "k8s-sa" {
-  description = "Service account for the Managed Service for Kubernetes cluster and node group."
+  description = "Service account for the Managed Service for Kubernetes cluster and node group"
   name        = "k8s-sa"
 }
 
@@ -117,7 +116,7 @@ resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
 }
 
 resource "yandex_kubernetes_cluster" "k8s-cluster" {
-  description = "Managed Service for Kubernetes cluster."
+  description = "Managed Service for Kubernetes cluster"
   name        = "k8s-cluster"
   network_id  = yandex_vpc_network.k8s-network.id
 
@@ -141,7 +140,7 @@ resource "yandex_kubernetes_cluster" "k8s-cluster" {
 }
 
 resource "yandex_kubernetes_node_group" "k8s-node-group" {
-  description = "Node group for the Managed Service for Kubernetes cluster."
+  description = "Node group for the Managed Service for Kubernetes cluster"
   cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
   name        = "k8s-node-group"
   version     = local.k8s_node_group_version
