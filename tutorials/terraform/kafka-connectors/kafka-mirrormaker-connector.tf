52d869350f480879c08c90b1b877808a36613cb8
vibelyakov <vibelyakov@yandex-team.ru>
2022-06-15 20:30:31 +0400

    YCDOCIO-1659: edit after review

--- tutorials/terraform/kafka-connectors/kafka-mirrormaker-connector.tf
+++ tutorials/terraform/kafka-connectors/kafka-mirrormaker-connector.tf
@@ -1,4 +1,4 @@
-# Infrastructure for Yandex Cloud Managed Service for Kafka and MirrorMaker connector
+# Infrastructure for Yandex Cloud Managed Service for Apache Kafka® and MirrorMaker connector
 #
 # RU: https://cloud.yandex.ru/docs/managed-kafka/tutorials/mirrormaker-unmanaged-topics
 # EN: https://cloud.yandex.com/en/docs/managed-kafka/tutorials/mirrormaker-unmanaged-topics
@@ -8,14 +8,15 @@
 # Network
 
 locals {
-  source_admin      = ""                                  # source cluster admin account name
-  source_password   = ""                                  # source cluster admin account password
-  target_admin      = ""                                  # target cluster admin account name
-  target_password   = ""                                  # target cluster admin account password
-  topics            = "data.*"                            # Specify topics that must be to migrated
-  source_alias      = "source"                            # Specify prefix for the source cluster
-  target_alias      = "target"                            # Specify prefix for the target cluster
-  bootstrap_servers = "somebroker1:9091,somebroker2:9091" # Specify bootstrap servers to connect to cluster
+  source_admin             = ""                          # source cluster admin account name.
+  source_password          = ""                          # source cluster admin account password.
+  source_alias             = "source"                    # Specify prefix for the source cluster.
+  source_bootstrap_servers = "<FQDN1>:9091,<FQDN2>:9091" # Specify bootstrap servers to connect to cluster.
+  target_admin             = ""                          # target cluster admin account name.
+  target_password          = ""                          # target cluster admin account password.
+  target_alias             = "target"                    # Specify prefix for the target cluster.
+  topics_prefix            = "data.*"                    # Specify topics that must be migrated.
+
 }
 
 variable "kafka_version" {
@@ -23,39 +24,39 @@ variable "kafka_version" {
 }
 
 resource "yandex_vpc_network" "network" {
-  description = "Network for Managed Service for Kafka and VM"
+  description = "Network for the Managed Service for Apache Kafka® cluster and VM"
   name        = "network"
 }
 
-# Subnet in ru-central1-a availability zone
+
 resource "yandex_vpc_subnet" "subnet-a" {
+  description    = "Subnet in ru-central1-a availability zone"
   name           = "subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.1.0.0/16"]
 }
 
-# Subnet in ru-central1-b availability zone
 resource "yandex_vpc_subnet" "subnet-b" {
+  description    = "Subnet in ru-central1-b availability zone"
   name           = "subnet-b"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.2.0.0/16"]
 }
 
-# Subnet in ru-central1-c availability zone
 resource "yandex_vpc_subnet" "subnet-c" {
+  description    = "Subnet in ru-central1-c availability zone"
   name           = "subnet-c"
   zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.3.0.0/16"]
 }
 
-# Security group for Managed Service for Kafka
 resource "yandex_vpc_default_security_group" "security-group" {
-  network_id = yandex_vpc_network.network.id
+  description = "Security group for Managed Service for the Managed Service for Apache Kafka® cluster"
+  network_id  = yandex_vpc_network.network.id
 
-  # Allow connections to Kafka cluster from the Internet
   ingress {
     description    = "Allow connections to Kafka cluster from the Internet"
     protocol       = "TCP"
@@ -73,8 +74,8 @@ resource "yandex_vpc_default_security_group" "security-group" {
   }
 }
 
-# Yandex Managed Service for Apache Kafka® cluster
 resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
+  description        = "Yandex Managed Service for the Managed Service for Apache Kafka® cluster"
   name               = "kafka-cluster"
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
@@ -82,14 +83,14 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
 
   config {
     brokers_count    = 1
-    version          = var.kafka_version
+    version          = local.kafka_version
     zones            = ["ru-central1-a"]
-    unmanaged_topics = true # Topic management via the Admin API
+    unmanaged_topics = true # Topic management via the Admin API.
     kafka {
       resources {
         disk_size          = 10 # GB
         disk_type_id       = "network-hdd"
-        resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM
+        resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM.
       }
       kafka_config {
         auto_create_topics_enable = true
@@ -113,19 +114,18 @@ resource "yandex_mdb_kafka_connector" "connector" {
   name        = "replication"
   tasks_max   = 3
   properties = {
-  #  refresh.topics.enabled = "true"
   }
   connector_config_mirrormaker {
-    topics             = local.topics
+    topics             = local.topics_prefix
     replication_factor = 1
     source_cluster {
       alias = local.source_alias
       external_cluster {
-        bootstrap_servers = local.bootstrap_servers
+        bootstrap_servers = local.source_bootstrap_servers
         sasl_username     = local.source_admin
         sasl_password     = local.source_password
-        sasl_mechanism    = "SCRAM-SHA-512" # Specify encryption algorythm for username and password
-        security_protocol = "SASL_SSL"      # Specify connection protocol for the MirrorMaker connector
+        sasl_mechanism    = "SCRAM-SHA-512" # Specify encryption algorythm for username and password.
+        security_protocol = "SASL_SSL"      # Specify connection protocol for the MirrorMaker connector.
       }
     }
     target_cluster {
