82e5a7b8e5efbb5d0fe8e0357e2fb31f8c886687
vibelyakov <vibelyakov@yandex-team.ru>
2022-06-30 17:21:57 +0400

    YCDOCIO-1733: edit after review

--- tutorials/terraform/managed-kubernetes/k8s-calico.tf
+++ tutorials/terraform/managed-kubernetes/k8s-calico.tf
@@ -3,11 +3,10 @@
 # Set the configuration of the Managed Service for Kubernetes cluster.
 
 locals {
-  folder_id              = ""            # Set your cloud folder ID.
-  k8s_node_group_version = "1.20"        # Set the version of Kubernetes for the node group.
-  k8s_cluster_version    = "1.20"        # Set the version of Kubernetes for the master host.
-  zone_a_v4_cidr_blocks  = "10.1.0.0/16" # Set the CIDR block for subnet.
-  sa_name                = ""
+  folder_id             = ""            # Set your cloud folder ID.
+  k8s_version           = "1.20"        # Set the version of Kubernetes for the master node and node group.
+  zone_a_v4_cidr_blocks = "10.1.0.0/16" # Set the CIDR block for subnet.
+  sa_name               = ""
 }
 
 resource "yandex_vpc_network" "k8s-network" {
@@ -27,58 +26,72 @@ resource "yandex_vpc_security_group" "k8s-main-sg" {
   description = "Security group for the Managed Service for Kubernetes cluster"
   name        = "k8s-main-sg"
   network_id  = yandex_vpc_network.k8s-network.id
+}
 
-  ingress {
-    description       = "The rule allows availability checks from the load balancer's range of addresses"
-    protocol          = "TCP"
-    predefined_target = "loadbalancer_healthchecks" # The load balancer's address range.
-    from_port         = 0
-    to_port           = 65535
-  }
+resource "yandex_vpc_security_group_rule" "loadbalancer" {
+  description       = "The rule allows availability checks from the load balancer's range of addresses"
+  direction         = "ingress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol          = "TCP"
+  predefined_target = "loadbalancer_healthchecks" # The load balancer's address range.
+  from_port         = 0
+  to_port           = 65535
+}
 
-  ingress {
-    description       = "The rule allows the master-node and node-node interaction within the security group"
-    protocol          = "ANY"
-    predefined_target = "self_security_group"
-    from_port         = 0
-    to_port           = 65535
-  }
+resource "yandex_vpc_security_group_rule" "node-interaction" {
+  description       = "The rule allows the master-node and node-node interaction within the security group"
+  direction         = "ingress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol          = "ANY"
+  predefined_target = "self_security_group"
+  from_port         = 0
+  to_port           = 65535
+}
 
-  ingress {
-    description    = "The rule allows the pod-pod and service-service interaction"
-    protocol       = "ANY"
-    v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
-    from_port      = 0
-    to_port        = 65535
-  }
+resource "yandex_vpc_security_group_rule" "pod-service-interaction" {
+  description    = "The rule allows the pod-pod and service-service interaction"
+  direction      = "ingress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol       = "ANY"
+  v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
+  from_port      = 0
+  to_port        = 65535
+}
 
-  ingress {
-    description    = "The rule allows receipt of debugging ICMP packets from internal subnets"
-    protocol       = "ICMP"
-    v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
-  }
+resource "yandex_vpc_security_group_rule" "ICMP-debug" {
+  description    = "The rule allows receipt of debugging ICMP packets from internal subnets"
+  direction      = "ingress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol       = "ICMP"
+  v4_cidr_blocks = [local.zone_a_v4_cidr_blocks]
+}
 
-  ingress {
-    description    = "The rule allows connection to Kubernetes API on 6443 port from the Internet"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 6443
-  }
+resource "yandex_vpc_security_group_rule" "port-6443" {
+  description    = "The rule allows connection to Kubernetes API on 6443 port from the Internet"
+  direction      = "ingress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol       = "TCP"
+  v4_cidr_blocks = ["0.0.0.0/0"]
+  port           = 6443
+}
 
-  ingress {
-    description    = "The rule allows connection to Kubernetes API on 443 port from the Internet"
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 443
-  }
+resource "yandex_vpc_security_group_rule" "port-443" {
+  description    = "The rule allows connection to Kubernetes API on 443 port from the Internet"
+  direction      = "ingress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol       = "TCP"
+  v4_cidr_blocks = ["0.0.0.0/0"]
+  port           = 443
+}
 
-  egress {
-    description    = "The rule allows all outgoing traffic"
-    protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 0
-    to_port        = 65535
-  }
+resource "yandex_vpc_security_group_rule" "outgoing-traffic" {
+  description    = "The rule allows all outgoing traffic"
+  direction      = "egress"
+  security_group_binding = yandex_vpc_security_group.k8s-main-sg.id
+  protocol       = "ANY"
+  v4_cidr_blocks = ["0.0.0.0/0"]
+  from_port      = 0
+  to_port        = 65535
 }
 
 resource "yandex_iam_service_account" "k8s-sa" {
@@ -86,13 +99,9 @@ resource "yandex_iam_service_account" "k8s-sa" {
   name        = local.sa_name
 }
 
-data "yandex_resourcemanager_folder" "cloud-folder" {
-  folder_id = local.folder_id # Folder ID required for binding roles to service account.
-}
-
 # Assign "editor" role to service account.
 resource "yandex_resourcemanager_folder_iam_binding" "editor" {
-  folder_id = data.yandex_resourcemanager_folder.cloud-folder.id
+  folder_id = local.folder_id
   role      = "editor"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-sa.id}"
@@ -101,7 +110,7 @@ resource "yandex_resourcemanager_folder_iam_binding" "editor" {
 
 # Assign "container-registry.images.puller" role to service account.
 resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
-  folder_id = data.yandex_resourcemanager_folder.cloud-folder.id
+  folder_id = local.folder_id
   role      = "container-registry.images.puller"
   members = [
     "serviceAccount:${yandex_iam_service_account.k8s-sa.id}"
@@ -114,7 +123,7 @@ resource "yandex_kubernetes_cluster" "k8s-cluster" {
   network_id  = yandex_vpc_network.k8s-network.id
 
   master {
-    version = local.k8s_cluster_version
+    version = local.k8s_version
     zonal {
       zone      = yandex_vpc_subnet.subnet-a.zone
       subnet_id = yandex_vpc_subnet.subnet-a.id
@@ -137,7 +146,7 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
   description = "Node group for the Managed Service for Kubernetes cluster"
   name        = "k8s-node-group"
   cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
-  version     = local.k8s_node_group_version
+  version     = local.k8s_version
 
   scale_policy {
     fixed_scale {
