ad4b3cd8a6fb5fd2e8d7284933e70b35f529a7fa
vibelyakov <vibelyakov@yandex-team.ru>
2022-08-08 19:49:40 +0400

    YCDOCIO-1706: improved data-from-kafka-to-clickhouse.tf

--- tutorials/terraform/data-from-kafka-to-clickhouse/data-from-kafka-to-clickhouse.tf
+++ tutorials/terraform/data-from-kafka-to-clickhouse/data-from-kafka-to-clickhouse.tf
@@ -5,49 +5,62 @@
 #
 # Set the configuration of the Managed Service for Apache Kafka® and ClickHouse clusters
 
+# Set the following settings:
 
-resource "yandex_vpc_network" "network" {
-  name        = "network"
-  description = "Network for the Managed Service for Apache Kafka® and ClickHouse clusters."
+locals {
+  producer_name     = "" # Set name of the producer.
+  producer_password = "" # Set the password of the producer.
+  topic_name        = "" # Set the Kafka topic name. Each Managed Service for Apache Kafka® cluster must have its unique topic name.
+  consumer_name     = "" # Set name of the consumer.
+  consumer_password = "" # Set the password of the consumer.
+  db_user_name      = "" # Set database username.
+  db_user_password  = "" # Set database user password.
 }
 
-# Subnet in ru-central1-a availability zone
+
+resource "yandex_vpc_network" "network" {
+  description = "Network for the Managed Service for Apache Kafka® and ClickHouse clusters"
+  name        = "network"
+}
+
 resource "yandex_vpc_subnet" "subnet-a" {
+  description    = "Subnet in the ru-central1-a availability zone"
   name           = "subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.1.0.0/16"]
 }
 
-# Security group for the Managed Service for Apache Kafka® and ClickHouse clusters
+
 resource "yandex_vpc_default_security_group" "security-group" {
-  network_id = yandex_vpc_network.network.id
+  description = "Security group for the Managed Service for Apache Kafka® and ClickHouse clusters"
+  network_id  = yandex_vpc_network.network.id
 
   ingress {
-    protocol       = "TCP"
     description    = "Allow connections to the Managed Service for Apache Kafka® cluster from the Internet"
+    protocol       = "TCP"
     port           = 9091
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol       = "TCP"
     description    = "# Allow connections to the Managed Service for ClickHouse cluster from the Internet"
+    protocol       = "TCP"
     port           = 9440
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    protocol       = "ANY"
     description    = "Allow outgoing connections to any required resource"
+    protocol       = "ANY"
     from_port      = 0
     to_port        = 65535
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Managed Service for Apache Kafka® cluster
 resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
+  description        = "Managed Service for Apache Kafka® cluster"
   name               = "kafka-cluster"
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
@@ -68,19 +81,19 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
   }
 
   user {
-    name     = "" # Set name of the producer
-    password = "" # Set the password of the producer
+    name     = local.producer_name
+    password = local.producer_password
     permission {
-      topic_name = "" # Topic name from line 94
+      topic_name = local.topic_name
       role       = "ACCESS_ROLE_PRODUCER"
     }
   }
 
   user {
-    name     = "" # Set name of the consumer
-    password = "" # Set the password of the consumer
+    name     = local.consumer_name
+    password = local.consumer_password
     permission {
-      topic_name = "" # Topic name from line 94
+      topic_name = local.topic_name
       role       = "ACCESS_ROLE_CONSUMER"
     }
   }
@@ -88,13 +101,13 @@ resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
 
 resource "yandex_mdb_kafka_topic" "events" {
   cluster_id         = yandex_mdb_kafka_cluster.kafka-cluster.id
-  name               = "" # Set topic name. Topic names should not be repeated.
+  name               = local.topic_name
   partitions         = 4
   replication_factor = 1
 }
 
-# Managed Service for ClickHouse cluster
 resource "yandex_mdb_clickhouse_cluster" "clickhouse-cluster" {
+  description        = "Managed Service for ClickHouse cluster"
   name               = "clickhouse-cluster"
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.network.id
@@ -120,8 +133,8 @@ resource "yandex_mdb_clickhouse_cluster" "clickhouse-cluster" {
   }
 
   user {
-    name     = "" # Set username
-    password = "" # Set user password
+    name     = local.db_user_name
+    password = local.db_user_password
     permission {
       database_name = "db1"
     }
