595b3ea087835e73ae62d87ceaaad2ded6bd53c3
anatolykraev <anatolykraev@yandex-team.ru>
2022-08-09 12:46:00 +0300

    YCDOCIO-1927: update create 1c cluster turorial

--- tutorials/terraform/postgresql-1c.tf
+++ tutorials/terraform/postgresql-1c.tf
@@ -2,33 +2,39 @@
 #
 # RU: https://cloud.yandex.ru/docs/managed-postgresql/tutorials/1c-postgresql
 # EN: https://cloud.yandex.com/en/docs/managed-postgresql/tutorials/1c-postgresql
-#
-# Set the user password for Managed Service for PostgreSQL 1C cluster
 
-
-resource "yandex_vpc_network" "postgresql-1c-network" {
-  name        = "postgresql-1c-network"
-  description = "Network for the Managed Service for PostgreSQL 1C cluster."
+# Set the configuration of the Managed Service for PostgreSQL 1C cluster:
+locals {
+  cluster_name = "" # Set a PostgreSQL 1C cluster name.
+  pg_version   = "" # Set a PostgreSQL 1C version.
+  db_name      = "" # Set a database name.
+  username     = "" # Set a user name.
+  password     = "" # Set a user password.
 }
 
-# Subnet in ru-central1-a availability zone
+resource "yandex_vpc_network" "postgresql-1c-network" {
+  description = "Network for the Managed Service for PostgreSQL 1C cluster"
+  name        = "postgresql-1c-network"
+}
+
 resource "yandex_vpc_subnet" "subnet-a" {
+  description    = "Subnet in the ru-central1-a availability zone"
   name           = "postgresql-subnet-a"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.postgresql-1c-network.id
   v4_cidr_blocks = ["10.1.0.0/16"]
 }
 
-# Subnet in ru-central1-b availability zone
 resource "yandex_vpc_subnet" "subnet-b" {
+  description    = "Subnet in the ru-central1-b availability zone"
   name           = "postgresql-subnet-b"
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.postgresql-1c-network.id
   v4_cidr_blocks = ["10.2.0.0/16"]
 }
 
-# Subnet in ru-central1-c availability zone
 resource "yandex_vpc_subnet" "subnet-c" {
+  description    = "Subnet in the ru-central1-c availability zone"
   name           = "postgresql-subnet-c"
   zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.postgresql-1c-network.id
@@ -36,26 +42,27 @@ resource "yandex_vpc_subnet" "subnet-c" {
 }
 
 # Security group for the Managed Service for PostgreSQL 1C cluster
-resource "yandex_vpc_default_security_group" "postgresql-security-group" {
-  network_id = yandex_vpc_network.postgresql-1c-network.id
+resource "yandex_vpc_security_group" "postgresql-security-group" {
+  description = "Security group for the Managed Service for PostgreSQL 1C cluster"
+  network_id  = yandex_vpc_network.postgresql-1c-network.id
 
   ingress {
+    description    = "Allow connections to cluster from the Internet"
     protocol       = "TCP"
-    description    = "Allow incoming connections to cluster from the Internet"
     port           = 6432
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Managed Service for PostgreSQL 1C cluster
 resource "yandex_mdb_postgresql_cluster" "postgresql-1c" {
-  name               = "postgresql-1c"
+  description        = "Managed Service for PostgreSQL 1C cluster"
+  name               = local.cluster_name
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.postgresql-1c-network.id
-  security_group_ids = [yandex_vpc_default_security_group.postgresql-security-group.id]
+  security_group_ids = [yandex_vpc_security_group.postgresql-security-group.id]
 
   config {
-    version = "12-1c"
+    version = local.pg_version
     resources {
       resource_preset_id = "s2.small"
       disk_type_id       = "network-ssd"
@@ -64,15 +71,15 @@ resource "yandex_mdb_postgresql_cluster" "postgresql-1c" {
   }
 
   database {
-    name  = "postgresql-1c"
-    owner = "user-1c" # Base owner name
+    name  = local.db_name
+    owner = local.username
   }
 
   user {
-    name     = "user-1c" # Username
-    password = ""        # Set user password
+    name     = local.username
+    password = local.password
     permission {
-      database_name = "postgresql-1c"
+      database_name = local.db_name
     }
   }
 
@@ -93,4 +100,16 @@ resource "yandex_mdb_postgresql_cluster" "postgresql-1c" {
     subnet_id        = yandex_vpc_subnet.subnet-c.id
     assign_public_ip = true # Required for connection from the Internet
   }
+}
+
+resource "yandex_mdb_postgresql_database" "db1c" {
+  cluster_id = yandex_mdb_postgresql_cluster.postgresql-1c.id
+  name       = local.db_name
+  owner      = yandex_mdb_postgresql_user.username.name
+}
+
+resource "yandex_mdb_postgresql_user" "username" {
+  cluster_id = yandex_mdb_postgresql_cluster.postgresql-1c.id
+  name       = local.username
+  password   = local.password
 }
