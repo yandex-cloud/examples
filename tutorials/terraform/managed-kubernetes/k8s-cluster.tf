516f7ad5a38eb173acf82300d6db7a46035b82c7
vibelyakov <vibelyakov@yandex-team.ru>
2022-08-16 20:14:14 +0400

    version fixes

--- tutorials/terraform/managed-kubernetes/k8s-cluster.tf
+++ tutorials/terraform/managed-kubernetes/k8s-cluster.tf
@@ -3,10 +3,9 @@
 # Set the configuration of the Managed Service for Kubernetes cluster.
 
 locals {
-  folder_id              = ""            # Set your cloud folder ID.
-  k8s_node_group_version = "1.20"        # Set the version of Kubernetes for the node group.
-  k8s_cluster_version    = "1.20"        # Set the version of Kubernetes for the master host.
-  zone_a_v4_cidr_blocks  = "10.1.0.0/16" # Set the CIDR block for subnet.
+  folder_id             = ""            # Set your cloud folder ID.
+  k8s_version           = "1.21"        # Set the version of Kubernetes.
+  zone_a_v4_cidr_blocks = "10.1.0.0/16" # Set the CIDR block for subnet.
 }
 
 resource "yandex_vpc_network" "k8s-network" {
@@ -113,7 +112,7 @@ resource "yandex_kubernetes_cluster" "k8s-cluster" {
   network_id  = yandex_vpc_network.k8s-network.id
 
   master {
-    version = local.k8s_cluster_version
+    version = local.k8s_version
     zonal {
       zone      = yandex_vpc_subnet.subnet-a.zone
       subnet_id = yandex_vpc_subnet.subnet-a.id
@@ -135,7 +134,7 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
   description = "Node group for the Managed Service for Kubernetes cluster"
   name        = "k8s-node-group"
   cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
-  version     = local.k8s_node_group_version
+  version     = local.k8s_version
 
   scale_policy {
     fixed_scale {
@@ -153,8 +152,8 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
     platform_id = "standard-v2" # Intel Cascade Lake
 
     network_interface {
-      nat        = true
-      subnet_ids = [yandex_vpc_subnet.subnet-a.id]
+      nat                = true
+      subnet_ids         = [yandex_vpc_subnet.subnet-a.id]
       security_group_ids = [yandex_vpc_security_group.k8s-main-sg.id]
     }
 
