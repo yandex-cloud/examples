35ee923ef764a690f52812a95d54f80518f783d6
anatolykraev <anatolykraev@yandex-team.ru>
2022-11-16 15:48:47 +0300

    YCDOCIO-1733: remove version k8s

--- tutorials/terraform/managed-kubernetes/k8s-calico.tf
+++ tutorials/terraform/managed-kubernetes/k8s-calico.tf
@@ -4,7 +4,6 @@
 
 locals {
   folder_id             = ""            # Set your cloud folder ID.
-  k8s_version           = "1.20"        # Set the version of Kubernetes for the master node and node group.
   zone_a_v4_cidr_blocks = "10.1.0.0/16" # Set the CIDR block for subnet.
   sa_name               = ""            # Set the service account name
 }
@@ -123,7 +122,6 @@ resource "yandex_kubernetes_cluster" "k8s-cluster" {
   network_id  = yandex_vpc_network.k8s-network.id
 
   master {
-    version = local.k8s_version
     zonal {
       zone      = yandex_vpc_subnet.subnet-a.zone
       subnet_id = yandex_vpc_subnet.subnet-a.id
@@ -146,7 +144,6 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
   description = "Node group for the Managed Service for Kubernetes cluster"
   name        = "k8s-node-group"
   cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
-  version     = local.k8s_version
 
   scale_policy {
     fixed_scale {
@@ -164,8 +161,8 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
     platform_id = "standard-v2" # Intel Cascade Lake
 
     network_interface {
-      nat        = true
-      subnet_ids = [yandex_vpc_subnet.subnet-a.id]
+      nat                = true
+      subnet_ids         = [yandex_vpc_subnet.subnet-a.id]
       security_group_ids = [yandex_vpc_security_group.k8s-main-sg.id]
     }
 
