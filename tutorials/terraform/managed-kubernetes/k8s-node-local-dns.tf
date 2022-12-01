6694e4e366a99a6edeb52272e0d326b05793b16b
anatolykraev <anatolykraev@yandex-team.ru>
2022-12-01 13:26:55 +0300

    YCDOCIO-1730: return k8s version

--- tutorials/terraform/managed-kubernetes/k8s-node-local-dns.tf
+++ tutorials/terraform/managed-kubernetes/k8s-node-local-dns.tf
@@ -7,6 +7,7 @@
 
 locals {
   folder_id             = ""            # Set your cloud folder ID.
+  k8s_version           = ""            # Set the Kubernetes version.
   zone_a_v4_cidr_blocks = "10.1.0.0/16" # Set the CIDR block for subnet.
   sa_name               = ""            # Set the service account name
 }
@@ -111,6 +112,7 @@ resource "yandex_kubernetes_cluster" "k8s-cluster" {
   network_id  = yandex_vpc_network.k8s-network.id
 
   master {
+    version = local.k8s_version
     zonal {
       zone      = yandex_vpc_subnet.subnet-a.zone
       subnet_id = yandex_vpc_subnet.subnet-a.id
@@ -132,6 +134,7 @@ resource "yandex_kubernetes_node_group" "k8s-node-group" {
   description = "Node group for the Managed Service for Kubernetes cluster"
   name        = "k8s-node-group"
   cluster_id  = yandex_kubernetes_cluster.k8s-cluster.id
+  version     = local.k8s_version
 
   scale_policy {
     fixed_scale {
