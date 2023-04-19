675060ab8e7ebca05a9cbb0a7e382bd6c40fad22
anatolykraev <anatolykraev@yandex-team.ru>
2023-04-19 11:39:00 +0300

    YCDOCIO-2563: add terraform file for mkf-mmy-data-transfer tutorial

---
+++ tutorials/terraform/data-transfer/data-transfer-mkf-mmy.tf
@@ -0,0 +1,167 @@
+# Infrastructure for the Yandex Cloud Managed Service for Apache Kafka®, Managed Service for MySQL and Data Transfer.
+#
+# RU: https://cloud.yandex.ru/docs/data-transfer/tutorials/data-transfer-mkf-mmy
+# EN: https://cloud.yandex.com/en/docs/data-transfer/tutorials/data-transfer-mkf-mmy
+#
+# Set source cluster and target cluster settings.
+locals {
+  # Source Managed Service for Apache Kafka® cluster settings:
+  source_kf_version    = "" # Set a desired version of Apache Kafka®. For available versions, see the documentation main page: https://cloud.yandex.com/en/docs/managed-kafka/
+  source_user_password = "" # Set a password for the user in the Managed Service for Apache Kafka® cluster.
+  source_endpoint_id   = "" # Set the source endpoint id.
+
+  # Target Managed Service for MySQL cluster settings:
+  target_mysql_version = "" # Set a desired version of MySQL.  For available versions, see the documentation main page: https://cloud.yandex.com/en/docs/managed-mysql/
+  target_user_password = "" # Set a password for the user in the Managed Service for MySQL cluster.
+
+  # Transfer settings:
+  transfer_enabled = 0 # Set to 1 to enable Transfer.
+}
+
+resource "yandex_vpc_network" "network" {
+  description = "Network for the Managed Service for Apache Kafka® and MySQL clusters"
+  name        = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet-a" {
+  description    = "Subnet in the ru-central1-a availability zone"
+  name           = "subnet-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["10.1.0.0/16"]
+}
+
+resource "yandex_vpc_security_group" "clusters-security-group" {
+  description = "Security group for the Managed Service for Apache Kafka and Managed Service for MySQL clusters"
+  network_id  = yandex_vpc_network.network.id
+
+  ingress {
+    description    = "Allow connections to the Managed Service for Apache Kafka® cluster from the Internet"
+    protocol       = "TCP"
+    port           = 9091
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description    = "Allow connections to the Managed Service for MySQL cluster from the Internet"
+    protocol       = "TCP"
+    port           = 3306
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description    = "The rule allows all outgoing traffic"
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
+resource "yandex_mdb_kafka_cluster" "kafka-cluster" {
+  description        = "Managed Service for Apache Kafka® cluster"
+  name               = "kafka-cluster"
+  environment        = "PRODUCTION"
+  network_id         = yandex_vpc_network.network.id
+  security_group_ids = [yandex_vpc_security_group.clusters-security-group.id]
+
+  config {
+    brokers_count    = 1
+    version          = local.source_kf_version
+    zones            = ["ru-central1-a"]
+    assign_public_ip = true # Required for connection from Internet
+    kafka {
+      resources {
+        resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM
+        disk_type_id       = "network-hdd"
+        disk_size          = 10 # GB
+      }
+    }
+  }
+
+  user {
+    name     = "mkf-user"
+    password = local.source_user_password
+    permission {
+      topic_name = "sensors"
+      role       = "ACCESS_ROLE_CONSUMER"
+    }
+    permission {
+      topic_name = "sensors"
+      role       = "ACCESS_ROLE_PRODUCER"
+    }
+  }
+}
+
+# Managed Service for Apache Kafka® topic
+resource "yandex_mdb_kafka_topic" "sensors" {
+  cluster_id         = yandex_mdb_kafka_cluster.kafka-cluster.id
+  name               = "sensors"
+  partitions         = 2
+  replication_factor = 1
+}
+
+resource "yandex_mdb_mysql_cluster" "mysql-cluster" {
+  description        = "Managed Service for MySQL cluster"
+  name               = "mysql-cluster"
+  environment        = "PRODUCTION"
+  network_id         = yandex_vpc_network.network.id
+  version            = local.target_mysql_version
+  security_group_ids = [yandex_vpc_security_group.clusters-security-group.id]
+
+  resources {
+    resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM
+    disk_type_id       = "network-hdd"
+    disk_size          = 10 # GB
+  }
+
+  host {
+    zone             = "ru-central1-a"
+    subnet_id        = yandex_vpc_subnet.subnet-a.id
+    assign_public_ip = true # Required for connection from Internet
+  }
+}
+
+# Managed Service for MySQL cluster database
+resource "yandex_mdb_mysql_database" "db1" {
+  cluster_id = yandex_mdb_mysql_cluster.mysql-cluster.id
+  name       = "db1"
+}
+
+# Managed Service for MySQL cluster user
+resource "yandex_mdb_mysql_user" "user1" {
+  cluster_id = yandex_mdb_mysql_cluster.mysql-cluster.id
+  name       = "mmy-user"
+  password   = local.target_user_password
+  permission {
+    database_name = yandex_mdb_mysql_database.db1.name
+    roles         = ["ALL"]
+  }
+}
+
+resource "yandex_datatransfer_endpoint" "mmy_target" {
+  count       = local.transfer_enabled
+  description = "Target endpoint for MySQL cluster"
+  name        = "mmy-target-tf"
+  settings {
+    mysql_target {
+      connection {
+        mdb_cluster_id = yandex_mdb_mysql_cluster.mysql-cluster.id
+      }
+      database = yandex_mdb_mysql_database.db1.name
+      user     = yandex_mdb_mysql_user.user1.name
+      password {
+        raw = local.target_user_password
+      }
+    }
+  }
+}
+
+resource "yandex_datatransfer_transfer" "mkf-mmy-transfer" {
+  count       = local.transfer_enabled
+  description = "Transfer from the Managed Service for Apache Kafka® to the Managed Service for MySQL"
+  name        = "transfer-from-mkf-to-mmy"
+  source_id   = local.source_endpoint_id
+  target_id   = yandex_datatransfer_endpoint.mmy_target[count.index].id
+  type        = "INCREMENT_ONLY" # Replication data.
+}
