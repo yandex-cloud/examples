9472074c56f67c96d92dcb5f23a4698221d84ffc
makushevkm <makushevkm@yandex-team.ru>
2023-06-21 01:28:06 +0600

    YCDOCIO-2736: Edit Kafka-ClickHouse manifest

--- tutorials/terraform/data-from-kafka-to-clickhouse/data-from-kafka-to-clickhouse.tf
+++ tutorials/terraform/data-from-kafka-to-clickhouse/data-from-kafka-to-clickhouse.tf
@@ -3,21 +3,23 @@
 # RU: https://cloud.yandex.ru/docs/managed-clickhouse/tutorials/fetch-data-from-mkf
 # EN: https://cloud.yandex.com/en/docs/managed-clickhouse/tutorials/fetch-data-from-mkf
 #
-# Set the configuration of the Managed Service for Apache Kafka® and ClickHouse clusters
-
-# Set the following settings:
+# Set the configuration of the Managed Service for Apache Kafka® and ClickHouse clusters.
 
 locals {
-  producer_name           = ""                   # Set name of the producer.
-  producer_password       = ""                   # Set the password of the producer.
-  topic_name              = ""                   # Set the Kafka topic name. Each Managed Service for Apache Kafka® cluster must have its unique topic name.
-  consumer_name           = ""                   # Set name of the consumer.
-  consumer_password       = ""                   # Set the password of the consumer.
-  db_user_name            = ""                   # Set database username.
-  db_user_password        = ""                   # Set database user password.
-  zone_a_v4_cidr_blocks   = "10.1.0.0/16"        # Set the CIDR block for subnet in the ru-central1-a availability zone.
-  kafka_cluster_name      = "kafka-cluster"      # Set the Managed Service for Apache Kafka® cluster name
-  clickhouse_cluster_name = "clickhouse-cluster" # Set the Managed Service for ClickHouse cluster name
+  # Specify the following settings:
+  producer_name     = "" # Name of a user with the producer role
+  producer_password = "" # Password of user with the producer role
+  topic_name        = "" # Apache Kafka® topic name. Each Managed Service for Apache Kafka® cluster must have its unique topic name.
+  consumer_name     = "" # Name of a user with the consumer role
+  consumer_password = "" # Password of user with the consumer role
+  db_user_name      = "" # Name of the database user
+  db_user_password  = "" # Password of the database user
+
+  # Change the following settings only if necessary
+
+  zone_a_v4_cidr_blocks   = "10.1.0.0/16"        # CIDR block for subnet in the ru-central1-a availability zone
+  kafka_cluster_name      = "kafka-cluster"      # Managed Service for Apache Kafka® cluster name. If you are going to create multiple clusters, then duplicate, rename, and edit this variable
+  clickhouse_cluster_name = "clickhouse-cluster" # Managed Service for ClickHouse cluster name
 }
 
 resource "yandex_vpc_network" "network" {
@@ -39,21 +41,21 @@ resource "yandex_vpc_default_security_group" "security-group" {
   network_id  = yandex_vpc_network.network.id
 
   ingress {
-    description    = "Allow connections to the Managed Service for Apache Kafka® cluster from the Internet"
+    description    = "Allows connections to the Managed Service for Apache Kafka® cluster from the Internet."
     protocol       = "TCP"
     port           = 9091
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description    = "# Allow connections to the Managed Service for ClickHouse cluster from the Internet"
+    description    = "Allows connections to the Managed Service for ClickHouse cluster from the Internet."
     protocol       = "TCP"
     port           = 9440
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    description    = "Allow outgoing connections to any required resource"
+    description    = "Allows outgoing connections to any required resource."
     protocol       = "ANY"
     from_port      = 0
     to_port        = 65535
@@ -121,6 +123,32 @@ resource "yandex_mdb_clickhouse_cluster" "clickhouse-cluster" {
       disk_type_id       = "network-ssd"
       disk_size          = 10 # GB
     }
+
+    # Uncomment the next block if you are going to use only one Managed Service for Apache Kafka® cluster.
+
+    #config {
+    #  kafka {
+    #    security_protocol = "SECURITY_PROTOCOL_SASL_SSL"
+    #    sasl_mechanism    = "SASL_MECHANISM_SCRAM_SHA_512"
+    #    sasl_username     = local.consumer_name
+    #    sasl_password     = local.consumer_password
+    #  }
+    #}
+
+    # Uncomment the next block if you are going to use multiple (N) Managed Service for Apache Kafka® clusters. Duplicate the block N times. Specify topic name and consumer credentials for every block.
+
+    #config {
+    #  kafka_topic {
+    #    name = "<topic name>"
+    #    settings {
+    #    security_protocol = "SECURITY_PROTOCOL_SASL_SSL"
+    #    sasl_mechanism    = "SASL_MECHANISM_SCRAM_SHA_512"
+    #    sasl_username     = "<name of the user for the consumer>"
+    #    sasl_password     = "<password of the user for the consumer>"
+    #    }
+    #  }
+    #}
+
   }
 
   host {
