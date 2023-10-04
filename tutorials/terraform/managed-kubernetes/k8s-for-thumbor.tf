9faf48d163a7f56cbdc9cc973fce714f604adfb6
eliseevaalena <eliseevaalena@yandex-team.ru>
2023-10-04 14:43:15 +0200

    YCDOCIO-2166: a correct comment

--- tutorials/terraform/managed-kubernetes/k8s-for-thumbor.tf
+++ tutorials/terraform/managed-kubernetes/k8s-for-thumbor.tf
@@ -214,7 +214,7 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = yandex_iam_service_account.storage-sa.id
 }
 
-// Create a bucket
+# Create a bucket
 resource "yandex_storage_bucket" "test" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
