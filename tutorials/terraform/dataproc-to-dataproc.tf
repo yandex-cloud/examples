0f7197f898c58c600cd6fcc5634860bec87c79a6
eliseevaalena <eliseevaalena@yandex-team.ru>
2024-04-15 13:11:10 +0200

    YCDOCIO-3535: added a cluster property for PySpark jobs

--- tutorials/terraform/dataproc-to-dataproc.tf
+++ tutorials/terraform/dataproc-to-dataproc.tf
@@ -161,6 +161,10 @@ resource "yandex_dataproc_cluster" "dataproc-source-cluster" {
     hadoop {
       services        = ["SPARK", "YARN"]
       ssh_public_keys = [file(local.dp_ssh_key)]
+      properties = {
+        # For PySpark jobs
+        "spark:spark.sql.hive.metastore.sharedPrefixes" = "com.amazonaws,ru.yandex.cloud"
+      }
     }
 
     subcluster_spec {
